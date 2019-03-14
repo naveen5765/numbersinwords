44748935d0ad65fb2b36a30ea23148e222e9773c
Naveen Anandhan <naveenanandhan@Naveens-MacBook-Pro.local>
2019-03-14 18:09:44 +0100

    test: Added describe block and initialized the component in beforeEach.

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,16 @@ import React from 'react';
 import TestUtils from 'react-dom/test-utils';
 import App from './App';
 
-it('show an input field to the user in order to enter the number', () => {
-  let app = TestUtils.renderIntoDocument(
-    <App />
-  );
-  
-  expect(TestUtils.findRenderedDOMComponentWithClass(app, 'input_numbers')).toBeTruthy();
-});
+describe("Number in Words Conversion", () => {
+  let app;
+  beforeEach(() =>{
+    app = TestUtils.renderIntoDocument(
+      <App />
+    );
+  });
+
+  it('show an input field to the user in order to enter the number', () => {
+    expect(TestUtils.findRenderedDOMComponentWithClass(app, 'input_numbers')).toBeTruthy();
+  });
+})
+

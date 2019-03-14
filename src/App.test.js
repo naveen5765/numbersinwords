7ef87f78c4c8143b540388fa8c17730776402218
Naveen Anandhan <naveenanandhan@Naveens-MacBook-Pro.local>
2019-03-14 19:18:22 +0100

    test: removed test-utils and used enzyme to render the dom for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import TestUtils from 'react-dom/test-utils';
+import { configure , shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+
+configure({ adapter: new Adapter() });
 
 describe("Number in Words Conversion", () => {
   let app;
   beforeEach(() =>{
-    app = TestUtils.renderIntoDocument(
-      <App />
-    );
+    app = shallow(<App />);
   });
 
   it('show an input field to the user in order to enter the number', () => {
-    expect(TestUtils.findRenderedDOMComponentWithClass(app, 'input_numbers')).toBeTruthy();
+    debugger;
+    expect(app.find('.input_numbers').length).toBe(1);
   });
+
+  // it('allow the user to enter only numbers', () => {
+    
+  //   expect()
+  // });
 })
 
